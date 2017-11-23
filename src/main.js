bb4ad27455ee5557c67cc2a9a2606b63047acc3b
Masayuki Morisaki <masayuki144@gmail.com>
2017-11-23 14:55:18 +0900

    Vuexストアを定義してVueに入れる

--- src/main.js
+++ src/main.js
@@ -1,13 +1,22 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import Vuex from 'vuex';
 import App from './App';
 import router from './router';
 
 Vue.config.productionTip = false;
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    msg: 'Hello Vuex Store.',
+  },
+});
 
 /* eslint-disable no-new */
 new Vue({
+  store,
   el: '#app',
   router,
   template: '<App/>',
