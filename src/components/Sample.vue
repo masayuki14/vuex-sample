5abda244d0401acfa6a102fb33ac7e7c10b68f66
Masayuki Morisaki <masayuki144@gmail.com>
2017-11-23 14:23:23 +0900

    Sample.vueを追加する

---
+++ src/components/Sample.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="sample">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'sample',
+  data() {
+    return {
+      msg: 'Welcome to My Vuex Sample.',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
