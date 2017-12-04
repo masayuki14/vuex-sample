896021abf7728fa23639163638c32c550ea7d4f6
Masayuki Morisaki <masayuki144@gmail.com>
2017-12-04 16:31:26 +0900

    storeを分割

--- src/main.js
+++ src/main.js
@@ -1,31 +1,11 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import Vuex from 'vuex';
 import App from './App';
 import router from './router';
-import * as types from './store/mutation-types';
+import store from './store';
 
 Vue.config.productionTip = false;
-Vue.use(Vuex);
-
-
-const store = new Vuex.Store({
-  state: {
-    msg: 'Hello Vuex Store.',
-  },
-  mutations: {
-    [types.UPDATE_MESSAGE](state, newMsg) {
-      state.msg = newMsg;
-    },
-  },
-  actions: {
-    repeat(context) {
-      let msg = context.state.msg
-      context.commit(types.UPDATE_MESSAGE, `${msg} ${msg}`)
-    },
-  }
-});
 
 /* eslint-disable no-new */
 new Vue({
